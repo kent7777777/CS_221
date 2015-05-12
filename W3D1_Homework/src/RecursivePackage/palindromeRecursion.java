0179163455f15b357d62e53e29afbb2953a4c74e
kent7777777 <coolkent7777777@gmail.com>
2015-05-12 10:09:05 -0500

    fixes

--- W3D1_Homework/src/RecursivePackage/palindromeRecursion.java
+++ W3D1_Homework/src/RecursivePackage/palindromeRecursion.java
@@ -10,9 +10,19 @@ package RecursivePackage;
  * @author Kevin
  */
 public class palindromeRecursion {
-    int num = 0;
+    
     public static boolean isPalindrome(String s){
-        
-        return isPalindrome
+        if(s.length() < 2){
+            return true;
+        }
+        if(s.charAt(0) != s.charAt(s.length() - 1)){
+            return false;
+        }
+        return isPalindrome(s.substring(1, s.length() - 1));
+    }
+    
+    public static void main(String[] args){
+        System.out.println(isPalindrome("abcba"));
     }
 }
+//true
